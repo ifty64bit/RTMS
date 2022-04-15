5ab1351ced899ef7df3fb5014380b49954d87697
ifty64bit <19-40447-1@student.aiub.edu>
2022-04-16 00:15:56 +0600

    fixredir

--- controller/common/redir.php
+++ controller/common/redir.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 if(!isset($_SESSION['user'])) {
-    header("Location: http://localhost/ERP");
+    header("Location: http://localhost/RTMS/");
 }
 ?>
--- view/SearchDriver.php
+++ view/SearchDriver.php
@@ -1,3 +1,4 @@
+<?php include("../controller/common/redir.php") ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
